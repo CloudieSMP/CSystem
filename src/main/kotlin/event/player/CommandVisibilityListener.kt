5a0de4697e55e673ee52bf03fc46f7671ed6a482
Sebastian <code@sebiann.moe>
2026-04-26 23:14:05 +0200

    Add AX, PAPI, Vanish and LP to Command Hidding
    Signed-off-by: Sebastian <code@sebiann.moe>

--- src/main/kotlin/event/player/CommandVisibilityListener.kt
+++ src/main/kotlin/event/player/CommandVisibilityListener.kt
@@ -16,14 +16,37 @@ class CommandVisibilityListener : Listener {
             "worldedit", "we", "tool", "toggleplace", "none", "brush", "br", ";", "/brush", "/br", "/sel", "/desel", "/deselect", "/toggleplace"
         )
 
+        private val AX_HIDDEN_COMMANDS = setOf(
+            "axir", "axinvrestore", "axinventoryrestore", "invrestore", "inventoryrestore"
+        )
+
+        private val PAPI_HIDDEN_COMMANDS = setOf(
+            "papi", "placeholderapi"
+        )
+
+        private val VANISH_HIDDEN_COMMANDS = setOf(
+            "vanish", "v", "pv", "premiumvanish"
+        )
+
+        private val LP_HIDDEN_COMMANDS = setOf(
+            "lp", "luckperms", "perm", "perms", "permission", "permissions"
+        )
+
         private val HIDDEN_COMMANDS = buildSet {
             addAll(WG_HIDDEN_COMMANDS)
             addAll(WG_HIDDEN_COMMANDS.map { "worldguard:$it" })
             addAll(WE_HIDDEN_COMMANDS)
             addAll(WE_HIDDEN_COMMANDS.map { "worldedit:$it" })
+            addAll(AX_HIDDEN_COMMANDS)
+            addAll(AX_HIDDEN_COMMANDS.map { "axinventoryrestore:$it" })
+            addAll(PAPI_HIDDEN_COMMANDS)
+            addAll(PAPI_HIDDEN_COMMANDS.map { "placeholderapi:$it" })
+            addAll(VANISH_HIDDEN_COMMANDS)
+            addAll(VANISH_HIDDEN_COMMANDS.map { "premiumvanish:$it" })
+            addAll(LP_HIDDEN_COMMANDS)
+            addAll(LP_HIDDEN_COMMANDS.map { "luckperms:$it" })
         }
     }
-
 
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onPlayerCommandSend(event: PlayerCommandSendEvent) {
