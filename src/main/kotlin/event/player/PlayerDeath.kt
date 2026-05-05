8db110702d45593c6fb90a9454d5be4a6c27a71e
Sebastian <code@sebiann.moe>
2026-05-05 16:06:50 +0200

    Add ChangeWorldEvent
    Signed-off-by: Sebastian <code@sebiann.moe>

--- src/main/kotlin/event/player/PlayerDeath.kt
+++ src/main/kotlin/event/player/PlayerDeath.kt
@@ -3,10 +3,18 @@ package event.player
 import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerChangedWorldEvent
 
 class PlayerDeath : Listener {
     @EventHandler
     private fun onPostRespawn(e: PlayerPostRespawnEvent) {
+        if (e.player.hasPermission("cloudie.group.admin")) {
+            e.player.sendOpLevel(2)
+        }
+    }
+
+    @EventHandler
+    private fun onChangedWorld(e: PlayerChangedWorldEvent) {
         if (e.player.hasPermission("cloudie.group.admin")) {
             e.player.sendOpLevel(2)
         }
